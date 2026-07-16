4e444de380dfc3dcac89d3a73868e726de52b715
hulk-the-great <121681734+hulk-the-great@users.noreply.github.com>
2026-07-16 10:43:00 -0500

    Add: pacific-atlantic-water-flow - submission-0

---
+++ Data Structures & Algorithms/pacific-atlantic-water-flow/submission-0.py
@@ -0,0 +1,47 @@
+from typing import List
+
+
+class Solution:
+    def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
+        rows = len(heights)
+        cols = len(heights[0])
+
+        pacific = set()
+        atlantic = set()
+
+        def dfs(r, c, visited, previous_height):
+            if (
+                (r, c) in visited
+                or r < 0
+                or c < 0
+                or r == rows
+                or c == cols
+                or heights[r][c] < previous_height
+            ):
+                return
+
+            visited.add((r, c))
+
+            dfs(r + 1, c, visited, heights[r][c])
+            dfs(r - 1, c, visited, heights[r][c])
+            dfs(r, c + 1, visited, heights[r][c])
+            dfs(r, c - 1, visited, heights[r][c])
+
+        # Top and bottom borders
+        for c in range(cols):
+            dfs(0, c, pacific, heights[0][c])
+            dfs(rows - 1, c, atlantic, heights[rows - 1][c])
+
+        # Left and right borders
+        for r in range(rows):
+            dfs(r, 0, pacific, heights[r][0])
+            dfs(r, cols - 1, atlantic, heights[r][cols - 1])
+
+        result = []
+
+        for r in range(rows):
+            for c in range(cols):
+                if (r, c) in pacific and (r, c) in atlantic:
+                    result.append([r, c])
+
+        return result
